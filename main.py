1d3ca7b32613817ebe4d771d24bd0cf3a10c413d
Allan <130999724+AllanAkkus@users.noreply.github.com>
2026-02-09 20:18:37 +0100

    Reapply "CLI IMPLEMENTED USING API"
    This reverts commit 66f2782f2d922bf75cba38040f704eac1ab63497.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import sys
+from pathlib import Path
+#SRC BEHÖVER SYNAS
+sys.path.append(str(Path(__file__).parent / "src"))
 
+from api.metals_client import MetalsClient
+from api.cache_manager import CacheManager
+from cli.commands import handle_command
+
+def main():
+    API_KEY = "demo
+    cache = CacheManager(ttl_minutes=15)
+    client = MetalsClient(api_key=API_KEY, cache=cache)
+    handle_command(client)
 
 if __name__ == "__main__":
-    pass
+    main()
